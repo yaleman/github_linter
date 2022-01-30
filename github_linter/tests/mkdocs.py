546bc8df2f0379dc760821dca072b5066f1da5d9
James Hodgkinson <james@terminaloutcomes.com>
2022-01-30 13:45:48 +1000

    closes #15 - mkdocs automation works now

--- github_linter/tests/mkdocs.py
+++ github_linter/tests/mkdocs.py
@@ -1,15 +1,63 @@
 """ does mkdocs things """
 
+from loguru import logger
+
+from github_linter.repolinter import RepoLinter
+from github_linter.utils import get_fix_file_path
+
 CATEGORY = "mkdocs"
 
 LANGUAGES = [
     "all",
 ]
 
-DEFAULT_CONFIG = None
+DEFAULT_CONFIG = {
+    "mkdocs_config_files" : [
+        "docs/mkdocs.yml",
+        "mkdocs.yml",
+    ],
+    "workflow_filepath" : ".github/workflows/mkdocs.yml"
+}
 
 
-# TODO: add a check for `docs/` and ("mkdocs.yml" or "docs/mkdocs.yml")
-# TODO: add fix to push mkdocs.yml from the fixes dir to ".github/workflows/mkdocs.yml"
+def needs_mkdocs_workflow(repo: RepoLinter):
+    """ checks that there's a mkdocs config in the repository """
+    for filepath in repo.config[CATEGORY]["mkdocs_config_files"]:
+        if repo.cached_get_file(filepath, clear_cache=True):
+            return True
+    return False
 
-# def fix_missing_mkdocs_workflow()
+def check_mkdocs_workflow_exists(repo: RepoLinter):
+    """ checks that the mkdocs github actions workflow exists """
+    if needs_mkdocs_workflow(repo):
+        if not repo.cached_get_file(repo.config[CATEGORY]["workflow_filepath"], clear_cache=True):
+            repo.error(CATEGORY, "MKDocs github actions configuration missing.")
+        # TODO: check if the file differs from expected.
+
+def fix_missing_mkdocs_workflow(repo: RepoLinter):
+    """ copies the mkdocs workflow if it needs it"""
+    if needs_mkdocs_workflow(repo):
+        workflow_file = repo.cached_get_file(repo.config[CATEGORY]["workflow_filepath"])
+        if workflow_file is None:
+            logger.debug("MKDocs workflow file doesn't exist, need to create it.")
+            commit_url = repo.create_or_update_file(
+                filepath=repo.config[CATEGORY]["workflow_filepath"],
+                newfile=get_fix_file_path(CATEGORY, "mkdocs.yml"),
+                message="github-linter.mkdocs created MKDocs github actions configuration",
+            )
+            repo.fix(CATEGORY, f"Created MKDocs github actions configuration: {commit_url}")
+        else:
+            # update it if it's different
+            fix_file = get_fix_file_path(CATEGORY, "mkdocs.yml")
+
+            if workflow_file.decoded_content == fix_file.read_bytes():
+                logger.debug("Don't need to update the workflow file!")
+                return
+            logger.error("Need to update the workflow file")
+            commit_url = repo.create_or_update_file(
+                filepath=repo.config[CATEGORY]["workflow_filepath"],
+                newfile=get_fix_file_path(CATEGORY, "mkdocs.yml"),
+                oldfile=workflow_file,
+                message="github-linter.mkdocs updated MKDocs github actions configuration",
+            )
+            repo.fix(CATEGORY, f"Updated MKDocs github actions configuration: {commit_url}")
