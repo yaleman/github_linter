33bb9b622f83c2b422544152bc81980abc84a953
James Hodgkinson <james@terminaloutcomes.com>
2022-02-15 08:18:07 +1000

    moving around testing a bit

--- run_pylint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "running pylint on module"
-poetry run python -m pylint --rcfile=.pylintrc github_linter/
-echo "running pylint on tests"
-poetry run python -m pylint --rcfile=.pylintrc test_*.py
