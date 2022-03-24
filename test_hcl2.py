e0083740b050ccf0ff2c52fea39d769e6f603b39
James Hodgkinson <james@terminaloutcomes.com>
2022-03-25 09:18:00 +1000

    bumping jinja2 and hcl2 version, testing hcl2 parsing

---
+++ test_hcl2.py
@@ -0,0 +1,19 @@
+""" tests things interacting with HCL-format files (terraform, hopefully)"""
+
+
+import hcl2  # type: ignore
+
+def test_load_basic_hcl2():
+    """ tests loading an example file """
+    examplefile = """terraform {
+    backend "s3" {
+        bucket = "example-bucket"
+        key        = "terraform.tfstate"
+        region = "us-east-1"
+    }
+}"""
+
+    value = hcl2.loads(examplefile)
+
+    assert value["terraform"]
+    print(value["terraform"])
