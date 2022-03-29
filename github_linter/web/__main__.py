fc980b314774ad9da3edf15f9bd163332c048d33
James Hodgkinson <james@terminaloutcomes.com>
2022-03-29 14:35:22 +1000

    adding bandit testing

--- github_linter/web/__main__.py
+++ github_linter/web/__main__.py
@@ -1,20 +1,26 @@
 """ cli for the web interface for the project that outgrew its intention """
 
+from typing import Optional
+
 import click
 import uvicorn # type: ignore
 
 @click.command()
 @click.option("--reload", is_flag=True)
 @click.option("--port", type=int, default=8000)
-@click.option("--host", type=str, default="0.0.0.0")
+@click.option("--host", type=str)
 @click.option("--proxy-headers", is_flag=True)
 def cli(
     reload: bool=False,
     port: int=8000,
-    host: str="0.0.0.0",
+    host: Optional[str]=None,
     proxy_headers: bool=False,
     ) -> None:
     """ github_linter server """
+
+    if host is None:
+        host="0.0.0.0" #nosec
+
     uvicorn.run(
         app="github_linter.web:app",
         reload=reload,
